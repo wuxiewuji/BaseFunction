1a879c520ac88d7129978be019936e29d8f88389
git_xxf <git_xxf@local.com>
2017-09-05 11:14:01 +0800

    基层vp搭建完成

--- baseui/src/main/java/com/tools/xxf/basefunction/rxbase/RxBaseActivity.java
+++ baseui/src/main/java/com/tools/xxf/basefunction/rxbase/RxBaseActivity.java
@@ -3,10 +3,16 @@ package com.tools.xxf.basefunction.rxbase;
 import android.os.Bundle;
 
 import com.tools.xxf.basefunction.BaseViewContract;
+import com.trello.rxlifecycle.ActivityEvent;
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 /**
  * TODO: 处理以为使用Rx导致内存泄漏而使用的 RxAppCompatActivity
@@ -28,6 +34,49 @@ public abstract class RxBaseActivity<T extends BaseViewContract.BasePresenter> e
         initViews(savedInstanceState);
         //初始化ToolBar
         initToolBar();
+        //模拟加载数据 用rx防止内存泄露
+        testRxJava();
+    }
+
+    private void testRxJava() {
+        //绑定方式一  同Activity生命周期
+        Observable.unsafeCreate(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                int i = 0;
+                while (i < 1000000000) {
+                    i++;
+                }
+                subscriber.onNext(String.valueOf(i));
+                subscriber.onCompleted();
+            }
+        }).compose(this.<String>bindToLifecycle())//指定同Activity同生命周期
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                    }
+                });
+        //绑定方式二  指定某个生命周期停止时间订阅
+        Observable.unsafeCreate(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                int i = 0;
+                while (i < 1000000000) {
+                    i++;
+                }
+                subscriber.onNext(String.valueOf(i));
+                subscriber.onCompleted();
+            }
+        }).compose(this.<String>bindUntilEvent(ActivityEvent.STOP))//指定STOP的时候停止订阅
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                    }
+                });
     }
 
     /**
